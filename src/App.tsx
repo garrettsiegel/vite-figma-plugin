569c19a6378726461da4200d248662372d0bd26f
garrettsiegel <garrett@garrettsiegel.com>
2025-03-24 23:36:03 -0400

    tailwind setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
-import styles from './App.module.css'
 import Count from './components/Count'
 
 function App() {
   return (
-    <main className={styles.plugin}>
-      <h1>Figma Plugin</h1>
+    <main className='bg-amber-600'>
+      <h1 className='underline text-3xl font-black'>Figma Plugin</h1>
       <h2>Rectangle Creator</h2>
       <Count />
     </main>
